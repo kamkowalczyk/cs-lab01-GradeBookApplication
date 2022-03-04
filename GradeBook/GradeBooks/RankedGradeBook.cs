c1714f418bec29d2a8d3de89209d4ee4701adf4a
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-03-04 11:40:39 +0100

    poprawione ostatnie testy. DONE

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,19 +12,19 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-        public override char GetLetterGrade(double avarageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count <5)
+            if (Students.Count <5 )
             {
                 throw new InvalidOperationException();
             }
-            if (avarageGrade >= 80)
+            if (averageGrade >= 80)
                 return 'A';
-            else if (avarageGrade < 80 && avarageGrade >= 60)
+            else if (averageGrade < 80 && averageGrade >= 60)
                 return 'B';
-            else if (avarageGrade < 60 && avarageGrade >= 40)
+            else if (averageGrade < 60 && averageGrade >= 40)
                 return 'C';
-            else if (avarageGrade < 40 && avarageGrade >= 20)
+            else if (averageGrade < 40 && averageGrade >= 20)
                 return 'D';
             else
                 return 'F';
