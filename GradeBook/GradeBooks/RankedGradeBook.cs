3b7e512463fb5e0c50aaeb294c3cd94161fa4435
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-03-04 10:38:29 +0100

    override CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,5 +29,17 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+           
+        }
     }
 }
